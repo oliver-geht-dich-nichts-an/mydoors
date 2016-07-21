170096939fb8e10d4556ef10d6506fb325b037d3
Oliver <superdaddy1963@googlemail.com>
2016-07-21 12:25:43 +0200

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,48 @@
+doors.register("door_aspen_wood", {
+		tiles = {{ name = "mydoors_door_aspen_wood.png", backface_culling = true }},
+		description = "Aspenwood Door",
+		inventory_image = "mydoors_item_aspen_wood.png",
+		groups = {choppy = 2, oddly_breakable_by_hand = 2, flammable = 2},
+		recipe = {
+			{"default:aspen_wood", "default:aspen_wood"},
+			{"default:aspen_wood", "default:aspen_wood"},
+			{"default:aspen_wood", "default:aspen_wood"},
+		}
+})
+
+doors.register("door_wood_jungle", {
+		tiles = {{ name = "mydoors_door_jungle_wood.png", backface_culling = true }},
+		description = "Junglewood Door",
+		inventory_image = "mydoors_item_jungle_wood.png",
+		groups = {choppy = 2, oddly_breakable_by_hand = 2, flammable = 2},
+		recipe = {
+			{"default:junglewood", "default:junglewood"},
+			{"default:junglewood", "default:junglewood"},
+			{"default:junglewood", "default:junglewood"},
+		}
+})
+
+doors.register("door_wood_pine", {
+		tiles = {{ name = "mydoors_door_pine_wood.png", backface_culling = true }},
+		description = "Pinewood Door",
+		inventory_image = "mydoors_item_pine_wood.png",
+		groups = {choppy = 2, oddly_breakable_by_hand = 2, flammable = 2},
+		recipe = {
+			{"default:pine_wood", "default:pine_wood"},
+			{"default:pine_wood", "default:pine_wood"},
+			{"default:pine_wood", "default:pine_wood"},
+		}
+})
+
+doors.register("door_wood_acacia", {
+		tiles = {{ name = "mydoors_door_acacia_wood.png", backface_culling = true }},
+		description = "Acaciawood Door",
+		inventory_image = "mydoors_item_acacia_wood.png",
+		groups = {choppy = 2, oddly_breakable_by_hand = 2, flammable = 2},
+		recipe = {
+			{"default:acacia_wood", "default:acacia_wood"},
+			{"default:acacia_wood", "default:acacia_wood"},
+			{"default:acacia_wood", "default:acacia_wood"},
+		}
+})
+
